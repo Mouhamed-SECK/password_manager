0da491dd30d2c64fba325fd2598135df90acdcb4
Mouhamed27-99 <medsoseck@gmail.com>
2022-11-18 10:32:18 +0100

    feat: handle new user account generation

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -7,6 +7,13 @@ use App\Entity\Role;
 
 use App\Form\UserRegistrationType;
 use App\Repository\UserRepository;
+use App\Service\SendEmailService;
+use App\Service\JWTService;
+use App\Service\PasswordGenerator;
+
+
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,7 +29,7 @@ class AdminUserController extends AbstractController
     private $repository;
     private $manager;
 
-    public function __construct(UserRepository  $repository, EntityManagerInterface $manager)
+    public function __construct(UserRepository  $repository, EntityManagerInterface $manager, JWTService $jwt)
     {
         $this->repository = $repository;
         $this->manager = $manager;
@@ -45,25 +52,44 @@ class AdminUserController extends AbstractController
 
 
     #[Route('/admin/users/new', name: 'admin.users.new')]
-    public function new(Request $request, EntityManagerInterface $manager,  UserPasswordEncoderInterface $encoder): Response
+    public function new(Request $request, EntityManagerInterface $manager, SendEmailService $mail, JWTService $jwt): Response
     {
 
         $user = new User();
 
 
 
-        $form =  $this->createForm(UserRegistrationType::class, $user);  
+        $form =  $this->createForm(UserRegistrationType::class, $user, );  
         $form->handleRequest($request) ;
 
         if ($form->isSubmitted() && $form->isValid()) { 
             $user->setLogin($user->getEmail());
-            $user->setPassword("momo");
-
-            $hash = $encoder->encodePassword($user,$user->getPassword());
-            $user->setPassword($hash);
-
+            //
+          
             $manager->persist($user);
             $manager->flush();
+
+
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            // On crée le Payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+
+            // On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            $mail->send(
+                'no-reply@myPassword.fr',
+                $user->getEmail(),
+                'Activation de votre compte sur MYPassword',
+                'register',
+                compact('user', 'token')
+            );
 
             return $this->redirectToRoute('admin.users.index');
 
@@ -75,4 +101,46 @@ class AdminUserController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+    public function verifyUser($token, JWTService $jwt, UserRepository $usersRepository, EntityManagerInterface $em, PasswordGenerator $passwordGenerator,  SendEmailService $mail, UserPasswordEncoderInterface $encoder): Response
+    {
+        //On vérifie si le token est valide, n'a pas expiré et n'a pas été modifié
+        if($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))){
+            // On récupère le payload
+            $payload = $jwt->getPayload($token);
+
+            // On récupère le user du token
+            $user = $usersRepository->find($payload['user_id']);
+
+          
+
+            //On vérifie que l'utilisateur existe et n'a pas encore activé son compte
+            if($user && !$user->isIsVerified()){
+
+                $temporaryPassword = $passwordGenerator->generate();
+                $hash = $encoder->encodePassword($user,$temporaryPassword);
+                $user->setPassword($hash);
+    
+                $user->setIsVerified(true);
+                $em->flush($user);
+
+                $mail->send(
+                    'no-reply@myPassword.fr',
+                    $user->getEmail(),
+                    'Votre mot de Pass temporaire',
+                    'temp_password',
+                    compact('user', 'temporaryPassword')
+                );
+                $this->addFlash('success', 'Utilisateur activé');
+                return $this->redirectToRoute('home');
+            }
+        }
+        // Ici un problème se pose dans le token
+        $this->addFlash('danger', 'Le token est invalide ou a expiré');
+        return $this->redirectToRoute('app_login');
+    }
+
 }
---
+++ src/Service/PasswordGenerator.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Service;
+
+class PasswordGenerator
+{
+
+    /**
+     * Génération du JWT
+     * @param array $header 
+     * @param array $payload 
+     * @param string $secret 
+     * @param int $validity 
+     * @return string 
+     */
+    public  function generate($length = 9, $add_dashes = false, $available_sets = 'luds'): string
+    {
+            $sets = array();
+        if(strpos($available_sets, 'l') !== false)
+            $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+        if(strpos($available_sets, 'u') !== false)
+            $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+        if(strpos($available_sets, 'd') !== false)
+            $sets[] = '23456789';
+        if(strpos($available_sets, 's') !== false)
+            $sets[] = '!@#$%&*?';
+
+        $all = '';
+        $password = '';
+        foreach($sets as $set)
+        {
+            $password .= $set[$this->tweak_array_rand(str_split($set))];
+            $all .= $set;
+        }
+
+        $all = str_split($all);
+        for($i = 0; $i < $length - count($sets); $i++)
+            $password .= $all[$this->tweak_array_rand($all)];
+
+        $password = str_shuffle($password);
+
+        if(!$add_dashes)
+            return $password;
+
+        $dash_len = floor(sqrt($length));
+        $dash_str = '';
+        while(strlen($password) > $dash_len)
+        {
+            $dash_str .= substr($password, 0, $dash_len) . '-';
+            $password = substr($password, $dash_len);
+        }
+        $dash_str .= $password;
+        return $dash_str;
+    }
+
+    
+
+    private function tweak_array_rand($array){
+        if (function_exists('random_int')) {
+            return random_int(0, count($array) - 1);
+        } elseif(function_exists('mt_rand')) {
+            return mt_rand(0, count($array) - 1);
+        } else {
+            return array_rand($array);
+        }
+    }
+}
